a597dc62f022c18d5b89479f95a2281fbf599f9c
zycrus <phantom.bot.202@gmail.com>
2022-08-21 19:30:48 +0800

    added main.py

---
+++ scripts/main.py
@@ -0,0 +1,20 @@
+import pygame, sys
+
+pygame.init()
+
+screen_width = 500
+screen_height = 500
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("You're not alone")
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill('black')
+
+    pygame.display.update()
+    clock.tick(60)
