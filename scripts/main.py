2450eec085926fad08928782e22e269ea4c69c43
washiwash <haynesmelchizedek15@gmail.com>
2022-08-22 13:17:19 +0800

    add player movement

--- scripts/main.py
+++ scripts/main.py
@@ -9,13 +9,78 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("You're not alone")
 clock = pygame.time.Clock()
 
+
+#Player and movement
+class Player:
+    def __init__(self, x, y):
+        self.x = int(x)
+        self.y = int(y)
+        self.rect = pygame.Rect(self.x, self.y, 32, 32)
+        self.color = (192,192,192)
+        self.velX = 0
+        self.velY = 0
+        self.left_pressed = False
+        self.right_pressed = False
+        self.up_pressed = False
+        self.down_pressed = False
+        self.speed = 4
+    
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, self.rect)
+    
+    def update(self):
+        self.velX = 0
+        self.velY = 0
+        if self.left_pressed and not self.right_pressed:
+            self.velX = -self.speed
+        if self.right_pressed and not self.left_pressed:
+            self.velX = self.speed
+        if self.up_pressed and not self.down_pressed:
+            self.velY = -self.speed
+        if self.down_pressed and not self.up_pressed:
+            self.velY = self.speed
+        
+        self.x += self.velX
+        self.y += self.velY
+
+        self.rect = pygame.Rect(int(self.x), int(self.y), 32, 32)
+
+#Player Initialization
+player = Player(screen_width/2, screen_height/2)
+
+
+        
+
+#Game Loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
-    screen.fill('black')
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.left_pressed = True
+            if event.key == pygame.K_RIGHT:
+                player.right_pressed = True
+            if event.key == pygame.K_UP:
+                player.up_pressed = True
+            if event.key == pygame.K_DOWN:
+                player.down_pressed = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                player.left_pressed = False
+            if event.key == pygame.K_RIGHT:
+                player.right_pressed = False
+            if event.key == pygame.K_UP:
+                player.up_pressed = False
+            if event.key == pygame.K_DOWN:
+                player.down_pressed = False
+        
+    #Character sample
+    screen.fill(((0,0,0)))  
+    player.draw(screen)
+    player.update()
+    pygame.display.flip()
 
     pygame.display.update()
     clock.tick(60)
